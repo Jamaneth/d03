6bff773ad9a43f71997223a02c5024229f1c7111
jamaneth <gregoire.virepinte@hec.edu>
2018-10-09 21:46:08 +0200

    Exercises for day 3

---
+++ ex04/django_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/gregoirevirepinte/Documents/GitHub/d03/ex04/django_venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- ex04/my_script.sh
+++ ex04/my_script.sh
@@ -1,9 +1,9 @@
-#mkdir django_venv
+#!/bin/sh
 
-#python3 -m venv --without-pip django_venv
+mkdir django_venv
+
+python -m venv django_venv
 conda create -n django_venv python=3.5.2 anaconda
 
-source activate django_venv
-#  pip3 install -r requirement.txt
-  conda install -n django_venv django=2.1.0
-  conda install -n django_venv psycopg2-binary==2.7.0
+source django_venv/bin/activate
+  pip3 install -r requirement.txt
